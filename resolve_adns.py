e2b2ffe83688ea67b9136251d6efd033c5e3d8d3
root <root@localhost.localdomain>
2013-08-27 22:40:23 +0800

    write unresolved host to a file

--- resolve_adns.py
+++ resolve_adns.py
@@ -4,53 +4,68 @@ import adns
 import sys
 import time
 
-def ReadFile(fr):
-    hosts=[]
-    file=open(fr)
-    lines=file.readlines()
+def read_file(fr):
+    hosts = []
+    file = open(fr)
+    lines = file.readlines()
     for line in lines:
         hosts.append(line[7:-2])
     file.close()
     return hosts
 
-def WriteFile(fw,ress):
+def write_file(fw,ress):
     file=open(fw,'a')
-    for res in ress:
-        #print res[0]
-        #print ress[res]
-        file.write(ress[res]+'		'+res[0]+'\n')
+    if isinstance(ress, dict): 
+        for res in ress:
+            #print res[0]
+            #print ress[res]
+            file.write(ress[res]+'		'+res[0]+'\n')
+    elif isinstance(ress, list):
+        for res in ress:
+            file.write(res + '\n')
+    else:
+        pass
     file.close()
 
 class QueryEngine(object):
 
     def __init__(self,s=None):
-        self._s=s or adns.init(adns.iflags.noautosys)
-        self._queries={}
-        self.IPhost={}
-
+        self._s = s or adns.init(adns.iflags.noautosys)
+        self._queries = {}
+        self.ip_host = {}
+        self.host_noip = []
+         
     def submit(self,qname,rr,flags=0):
         q=self._s.submit(qname,rr,flags)
-        self._queries[q]=qname,rr,flags
+        self._queries[q] = qname,rr,flags
 
-    def run(self,timeout=0):
+    def run(self,timeout = 0):
         for q in self._s.completed():
-            answer=q.check()
-            qname,rr,flags=self._queries[q]
+            answer = q.check()
+            qname,rr,flags = self._queries[q]
             del self._queries[q]
             if answer[3]:
-                self.IPhost[answer[3]]=qname
+                self.ip_host[answer[3]] = qname
             #print qname, '		',answer[3]
+            else:
+                #print 'no answer: ', qname
+                self.host_noip.append(qname)
+
 
     def finished(self):
         return not len(self._queries)
 
-    def getIPhost(self):
-        return self.IPhost
+    def get_ip_host(self):
+        return self.ip_host
 
-def resolveDns(fr,fw):
-    hosts=ReadFile(fr)
-    start=time.time()
-    qe=QueryEngine()
+    def get_host_noip(self):
+        print 'len(self.host_noip): ', len(self.host_noip)
+        return self.host_noip
+
+def resolveDns(fr,fw,intensity):
+    hosts = read_file(fr)
+    start = time.time()
+    qe = QueryEngine()
     for host in hosts:
         qe.submit(host,adns.rr.AAAA)
 
@@ -58,12 +73,17 @@ def resolveDns(fr,fw):
         qe.run()
 
     print 'time cost:',time.time()-start
-    IPhost=qe.getIPhost()
-    WriteFile(fw,IPhost)
-    return len(hosts), len(IPhost)
+    ip_host = qe.get_ip_host()
+    write_file(fw,ip_host)
+    host_noip = qe.get_host_noip()
+    #print 'len(host_noip): ', len(host_noip)
+    write_file(fw+'_failed_resolve', host_noip)    
+    return len(hosts), len(ip_host)
 
 if __name__=='__main__':
     filename=sys.argv[1]
-    host_cnt, IP_cnt = resolveDns(filename,'hostIP')
+    savename=sys.argv[2]
+    
+    host_cnt, IP_cnt = resolveDns(filename, savename, intensity = 500)
     print 'Resolve ' + unicode(host_cnt) + ' domains and get '\
           + unicode(IP_cnt) + ' IPv6 address.'
